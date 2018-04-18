857e59d1b90926455961ce52b9ef3fced74f265f
ealvarez <emilioa84@hotmail.com>
2018-04-17 22:54:14 -0700

    JS initial

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ $('video, audio').mediaelementplayer({
     startLanguage: 'en',
     skipBackInterval: 4
   });
+
+  const video = document.getElementById('myVideo');
+  const span = document.getElementsByTagName('span');
+
+
+  video.addEventListener('timeupdate', () => {
+    for (let i = 0; i < span.length; i++) {
+      let startTime = span[i].getAttribute('data-start');
+      let endTime = span[i].getAttribute('data-end');
+      let currentTime = video.currentTime;
+      if (currentTime > startTime && currentTime < endTime) {
+        span[i].style.color = "#FFA500";
+      } 
+      else {
+        span[i].style.color ="#000000";
+      }
+    }
+  });
